fcb789c194dfff768fc62a4ea8c9e28d8ef54120
babelarr <babelarr@gmail.com>
2019-07-06 21:07:12 +0200

    refact fileUpload

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,12 +3,23 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
 import FileUpload from './FileUpload';
 
 const styles = theme => ({
   galleryContainer: {
     backgroundColor: theme.palette.primary.dark,
     padding: theme.spacing.unit * 5
+  },
+  imgContainer:{
+    margin: theme.spacing.unit * 2
+  },
+
+  media: {
+    height: 200
+  },
+  buttonContainer:{
+    marginTop: theme.spacing.unit * 2
   }
 });
 
@@ -26,24 +37,23 @@ class Gallery extends Component {
               En este espacio podrás subir el dia de la boda las fotos que vayas haciendo y verás las del resto de los invitados.
             </Typography>
           </Grid>
-          <Grid item>
+          <Grid item className={classes.buttonContainer}>
             <FileUpload handleUpload={this.props.handleUpload} uploadValue={this.props.uploadValue} />
           </Grid>
-          {
-              this.props.pictures.map(picture => (
-                <Grid item key={picture.key}>
-                    <figure>
-                      <img
-                        src={picture.image}
-                        data-caption={`Imagen compartida por ${picture.displayName}`}
-                        alt='Ampliar/Reducir imagen' />
-                    </figure>
-                    <div>
-                      {picture.displayName}
-                    </div>
-                </Grid>
-              )).reverse()
-            }
+          <Grid container className={classes.imgContainer} spacing={16} >
+            {
+                this.props.pictures.map(picture => (
+                  <Grid item xs={6} key={picture.key}>
+                    <CardMedia
+                      className={classes.media}
+                      image= {picture.image}
+                      title={picture.displayName}
+                    />
+                  </Grid>
+              
+                )).reverse()
+              }
+            </Grid>
       </Grid>
     );
   }
